1155bde950fce04f1a6b5442494a69d69d79034a
mandresy andri <70854158+mandresyandri@users.noreply.github.com>
2024-04-03 15:12:26 +0200

    Update QueryByGenre.py

--- QueryByGenre.py
+++ QueryByGenre.py
@@ -3,58 +3,41 @@ import boto3
 from boto3.dynamodb.conditions import Key, Attr
 import argparse
 import time
-from decimal import *
 
-def MakeGSI():
+def scan_movies(dname):
     region=boto3.session.Session().region_name
     dynamodb = boto3.resource('dynamodb', region_name=region) #low-level Client
     table = dynamodb.Table('movies') #define which dynamodb table to access
 
-    response = table.update(
-        AttributeDefinitions=[
-            {
-                "AttributeName": "genre",
-                "AttributeType": "S"
-            },
-        ],
-        GlobalSecondaryIndexUpdates=[
-            {
-                'Create': {
-                    'IndexName': "genre-globo-index",
-                    'KeySchema': [
-                        {
-                            'AttributeName': "genre",
-                            'KeyType': 'HASH'
-                        }
-                    ],
-                    'Projection': {
-                        'ProjectionType': 'ALL'
-                    },
-                    'ProvisionedThroughput': {
-                        'ReadCapacityUnits': 1000,
-                        'WriteCapacityUnits': 1000
-                    }
-                }
-            }
-        ],
+    recordcount = 0
+    recordscannedcount = 0
+
+    scanreturn = table.scan(                    # perform first scan
+        FilterExpression=Attr("genre").eq(dname)
     )
+    recordcount += scanreturn['Count']
+    recordscannedcount += scanreturn['ScannedCount']
 
-    table.reload()       # this section to reload the table status every 10 seconds until the GSI finishes creating
-    tmpreply = table.global_secondary_indexes
-    indexnum = 0;
-    while tmpreply[indexnum]['IndexName'] != "genre-globo-index" :   # in case other indexes exist for this table, find the one we just created
-        indexnum += 1
-    while tmpreply[indexnum]['IndexStatus'] != 'ACTIVE':    # check to see if the new one is active yet, and if not... repeat until it is
-        time.sleep(10)
-        print("Still creating...")
-        table.reload()
-        tmpreply = table.global_secondary_indexes
+    while 'LastEvaluatedKey' in scanreturn.keys(): # if lastevaluatedkey is present, we need to keep scanning and adding to our counts until everything is scanned
+        scanreturn = table.scan(
+            FilterExpression=Attr("genre").eq(dname),
+            ExclusiveStartKey = scanreturn['LastEvaluatedKey']
+        )
+        recordcount += scanreturn['Count']
+        recordscannedcount += scanreturn['ScannedCount']
 
-    return response
+    return [recordcount, recordscannedcount]
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Genre", help="Search by genre, ex: Action... argument is case sensitive")
+    args = parser.parse_args()
+    query_direct = (args.Genre) #section to collect argument from command line
 
     start = time.time()
-    result = MakeGSI()
+    movies = scan_movies(query_direct) #scan_movies returns our total counts as two items of a list
     end = time.time()
+    print("Count is ", movies[0])   # print the count of items returned by the scan
+    print("ScannedCount is ", movies[1])  # print the count of items that had to be scanned to process the scan
     print('Total time: {} sec'.format(end - start))
